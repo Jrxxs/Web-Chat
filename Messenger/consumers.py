694c53f7b00926997cce4d0c3cd52bcbd6bbd442
Jrxxs <iliapihnov@gmail.com>
2022-10-03 23:20:17 +0300

    Rebuild consumer to async and add hub connection

--- Messenger/consumers.py
+++ Messenger/consumers.py
@@ -1,62 +1,14 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import User, Private_Log
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        client = text_data_json['author']
-        companion = text_data_json['destination']
-        date = text_data_json['date']
-        Client = User.objects.get(username=client)
-        Companion = User.objects.get(username=companion)
-        Private_Log.objects.create(From_User=Client, To_User=Companion, Message=message, Date_Time=date)
-
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'author': client,
-                'date': date
-            }
-        )
-
-    def chat_message(self, event):
-        message = event['message']
-        client = event['author']
-        date = event['date']
-
-        async_to_sync(self.send(text_data=json.dumps({
-            'message': message,
-            'author': client,
-            'date': date
-        })))
-
-
-class Hub(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # print(self.scope)
         self.room_group_name = 'room_%s' % self.room_name
 
         # Join room group
@@ -66,6 +18,10 @@ class Hub(AsyncWebsocketConsumer):
         )
 
         await self.accept()
+
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -78,21 +34,115 @@ class Hub(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        client = text_data_json['author']
+        companion = text_data_json['destination']
+        date = text_data_json['date']
+
+        Client = await self.get_user(username=client)
+        Companion = await self.get_user(username=companion)
+
+        await self.save_message(sender=Client, reciever=Companion, data=message, time=date)
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'author': client,
+                'date': date
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        client = event['author']
+        date = event['date']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'author': client,
+            'date': date
         }))
+
+    @sync_to_async
+    def get_user(self, username):
+        return User.objects.get(username=username)
+
+    @sync_to_async
+    def save_message(self, sender, reciever, data, time):
+        Private_Log.objects.create(From_User=sender, To_User=reciever, Message=data, Date_Time=time)
+
+
+
+class Hub(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # print(self.scope)
+        self.room_group_name = 'hub_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        client = text_data_json['author']
+        companion = text_data_json['destination']
+        date = text_data_json['date']
+
+        Client = await self.get_user(username=client)
+        Companion = await self.get_user(username=companion)
+
+        await self.save_message(sender=Client, reciever=Companion, data=message, time=date)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'author': client,
+                'date': date
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+        client = event['author']
+        date = event['date']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'author': client,
+            'date': date
+        }))
+
+    @sync_to_async
+    def get_user(self, username):
+        return User.objects.get(username=username)
+
+    @sync_to_async
+    def save_message(self, sender, reciever, data, time):
+        Private_Log.objects.create(From_User=sender, To_User=reciever, Message=data, Date_Time=time)
